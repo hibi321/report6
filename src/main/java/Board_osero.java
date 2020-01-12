acfb40b09f097a0e9a0acdf15b0db142755189dd
Hibiki Miyagi <e195710@ie.u-ryukyu.ac.jp>
2020-01-12 22:53:25 +0900

    Report6

--- src/main/java/Board_osero.java
+++ src/main/java/Board_osero.java
@@ -1,5 +1,26 @@
 public class Board_osero {
     String[][] board = new String[8][8];
 
+    String empty = " ";
+    String black = "●";
+    String white = "○";
+
+    Board_osero(String empty, String black, String white, String[][] board){
+        this.empty = empty;
+        this.black = black;
+        this.white = white;
+        this.board = board;
+
+    }
+
+
+    public String[][] initialise(){
+        for (int a = 0; a < 8; a++){
+            for (int b = 0; b < 8; b++){
+                board[a][b] = empty;
+            }
+        }
+        return board;
+    }
 
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]){
+        Board_osero c = new Board_osero(" ","●", "○", new String[8][8]);
+        c.initialise();
+        System.out.println(c.board);
+    }
+}
