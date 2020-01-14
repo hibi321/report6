a0bca25b4ce651e94ac6e03b7a94bc1b7f851899
Hibiki Miyagi <e195710@ie.u-ryukyu.ac.jp>
2020-01-14 21:02:02 +0900

    Report6

--- src/main/java/Board_osero.java
+++ src/main/java/Board_osero.java
@@ -1,10 +1,19 @@
+/**
+ * オセロゲームを作るためのコード
+ */
 public class Board_osero {
-    String[][] board = new String[8][8];
+    String[][] board;
+    String empty;
+    String black;
+    String white;
 
-    String empty = " ";
-    String black = "●";
-    String white = "○";
-
+    /**
+     * 変数に値を代入するためのコンストラクタ
+     * @param empty:空白、何も行かれていない場所
+     * @param black:黒の石が置かれている場所
+     * @param white:白の石が置かれている場所
+     * @param board:オセロのボードの大きさを決めるための多次元配列、基本は8✖︎8の大きさ
+     */
     Board_osero(String empty, String black, String white, String[][] board){
         this.empty = empty;
         this.black = black;
@@ -13,21 +22,22 @@ public class Board_osero {
 
     }
 
-
-    public String[][] initialise(){
+    /**
+     * ボードを初期状態にセットするためのメソッド
+     */
+    public void initialise(){
         for (int a = 0; a < 8; a++){
             for (int b = 0; b < 8; b++){
                 board[a][b] = empty;
             }
         }
-
         board[3][3] = black;
         board[3][4] = white;
         board[4][3] = white;
         board[4][4] = black;
-
-        return board;
     }
+
+
 
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String args[]){
         Board_osero c = new Board_osero(" ","●", "○", new String[8][8]);
         c.initialise();
-        System.out.println(Arrays.deepToString(c.board));
+        for(int i=0; i < 8; i++){
+            System.out.println(Arrays.toString(c.board[i]));
+        }
     }
 }
